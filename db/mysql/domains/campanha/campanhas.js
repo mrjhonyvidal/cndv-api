b64efc7d1789191c0e41a8e597934fe6cfcb3bff
Jhony Vidal <mrjhonyvidal@gmail.com>
2021-03-17 19:49:08 -0300

    Add descricao field to insert Campanha

--- db/mysql/domains/campanha/campanhas.js
+++ db/mysql/domains/campanha/campanhas.js
@@ -37,7 +37,7 @@ async function selectCampanhaById(id) {
 async function insertCampanha(campanha) {
     try{
         const conn      = await mysqlDb.connect();
-        const sql       = 'INSERT INTO campanhas(nome, idade_inicio, idade_final, cidade, uf) VALUES (?,?,?,?,?);';
+        const sql       = 'INSERT INTO campanhas(nome, idade_inicio, idade_final, cidade, uf, descricao) VALUES (?,?,?,?,?,?);';
         const values    = [
             campanha.nome,
             campanha.idade_inicio,
